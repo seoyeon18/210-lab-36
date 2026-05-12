73ac5a703cae0c33d4316576168091534a325cd3
Your Name <brightsy04@gmail.com>
2026-05-11 23:10:07 -0700

    Clean up menu output

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -5,18 +5,23 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     IntBinaryTree tree;
     ifstream inputFile("codes.txt");
 
-    if (!inputFile) {
+    if (!inputFile)
+    {
         cout << "Error: Could not open codes.txt" << endl;
         return 1;
     }
 
     string code;
+    string oldCode;
+    string newCode;
 
-    while (inputFile >> code) {
+    while (inputFile >> code)
+    {
         tree.insertNode(code);
     }
 
@@ -26,56 +31,90 @@ int main() {
     cout << "Codes in sorted order:";
     tree.displayInOrder();
 
-        int choice;
+    int choice;
 
-    do {
+    do
+    {
         showMenu();
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                cout << "Enter code to add:";
-                cin >> code;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter code to add:";
+            cin >> code;
 
-                if (tree.searchNode(code)) {
+            if (tree.searchNode(code))
+            {
                 cout << "That code already exists" << endl;
-                } else {
+            }
+            else
+            {
                 tree.insertNode(code);
-                }
-                break;
-            case 2:
-                case 2:
-                cout << "Enter code to delete: ";
-                cin >> code;
+            }
+            break;
+        case 2:
+        case 2:
+            cout << "Enter code to delete: ";
+            cin >> code;
 
-                if (tree.searchNode(code)) {
+            if (tree.searchNode(code))
+            {
                 tree.remove(code);
                 cout << "Code deleted" << endl;
-                } else {
+            }
+            else
+            {
                 cout << "Code not found" << endl;
-                }
-                break;
-            case 3:
-                 cout << "Enter code to search:";
-                cin >> code;
+            }
+            break;
+        case 3:
+            cout << "Enter code to search:";
+            cin >> code;
 
-                if (tree.searchNode(code)) {
+            if (tree.searchNode(code))
+            {
                 cout << "Code found." << endl;
-                } else {
+            }
+            else
+            {
                 cout << "Code not found." << endl;
+            }
+        case 4:
+            cout << "Enter code to modify: ";
+            cin >> oldCode;
+
+            if (tree.searchNode(oldCode))
+            {
+                cout << "Enter new code: ";
+                cin >> newCode;
+
+                if (tree.searchNode(newCode))
+                {
+                    cout << "New code already exists. Modify cancelled." << endl;
                 }
-            case 4:
-                cout << "Modify selected." << endl;
-                break;
-            case 5:
-                tree.displayInOrder();
-                break;
-            case 6:
-                cout << "Goodbye." << endl;
-                break;
-            default:
-                cout << "Invalid choice." << endl;
+                else
+                {
+                    tree.remove(oldCode);
+                    tree.insertNode(newCode);
+                    cout << "Code modified." << endl;
+                }
+            }
+            else
+            {
+                cout << "Original code not found." << endl;
+            }
+            break;
+        case 5:
+            cout << "\nCodes in sorted order:" << endl;
+            tree.displayInOrder();
+            break;
+        case 6:
+            cout << "Goodbye" << endl;
+            break;
+        default:
+            cout << "Invalid choice" << endl;
         }
 
     } while (choice != 6);
@@ -83,7 +122,8 @@ int main() {
     return 0;
 }
 
-void showMenu() {
+void showMenu()
+{
     cout << "========== Code BST Menu ==========" << endl;
     cout << "1. Add a code" << endl;
     cout << "2. Delete a code" << endl;
